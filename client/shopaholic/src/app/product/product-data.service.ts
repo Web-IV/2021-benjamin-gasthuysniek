54f94e2e8f07381002f184b8d77b25a5b1c640df
= <=>
2021-08-05 16:46:35 +0200

    adding changes to tests

--- client/shopaholic/src/app/product/product-data.service.ts
+++ client/shopaholic/src/app/product/product-data.service.ts
@@ -36,7 +36,10 @@ this.products$.subscribe((products: Product[])=> {
 
   handleError(err: any): Observable<never>{
     let errorMessage: string;
-    if (err instanceof HttpErrorResponse){
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    }
+    else if (err instanceof HttpErrorResponse){
       errorMessage = `'${err.status} ${err.statusText}' when accessing '${err.url}'`;
     }
       else{

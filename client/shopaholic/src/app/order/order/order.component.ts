469af1b2ed2c06053993d80d43269d6877822ea6
= <=>
2021-08-19 14:52:26 +0200

    showing orderlines of order

--- client/shopaholic/src/app/order/order/order.component.ts
+++ client/shopaholic/src/app/order/order/order.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Orderline } from 'src/app/orderline/orderline.model';
 import { ProductDataService } from 'src/app/product/product-data.service';
 import { Product } from 'src/app/product/product.model';
@@ -18,8 +18,8 @@ import { Order } from '../order.model';
 export class OrderComponent implements OnInit {  
   @Input() public order: Order;
    private currentUser: BehaviorSubject<string>;
-   public Order: Order;
-   private productId: number;
+ 
+   private productId: number;  
    private product: Product;
    //private productId: number;
   constructor(private route : ActivatedRoute, private orderDataService: OrderDataService, private authService: AuthenticationService, private productDataService: ProductDataService) {
@@ -51,24 +51,53 @@ export class OrderComponent implements OnInit {
       }
       );
       //this.orderlines.push( new Orderline(0,this.productId,1));
-      this.order = new Order(0,true,[],new Date(),this.product.unitPrice);
-    
-
-      console.log("printing currentuser");
-      console.log(this.currentUser);
       console.log("printing productid")
       console.log(this.productId);
+      this.productDataService.getProduct$(this.productId.toString()).subscribe((prod: Product) =>{
+        this.product = prod;
+        console.log("printing the product in getproduct");
+        console.log(this.product);
+        this.order = new Order(0,true,[],new Date(),this.product.unitPrice);
+        console.log("printing this.order before adding orderline");
+        console.log(this.order);
+        this.order.addOrderline(this.productId,1);
+        console.log("printing order after adding orderline");
+        console.log(this.order);
+      });
+
+      /*console.log("printing product after assigning in productdataservice.getproduct");
+      console.log(this.product);*/
+     
+      //this.order = new Order(0,true,[],new Date(),this.product.unitPrice);
+     /* console.log("printing this.order");
+      console.log(this.order);*/
+
+      /*console.log("printing currentuser");
+      console.log(this.currentUser);*/
+     
      /* console.log("printing orderlines");
       console.log(this.orderlines);*/
-      console.log("printing product");
-      console.log(this.product);
-      console.log("printing this.order");
-      console.log(this.order);
+     /* console.log("printing product");
+      console.log(this.product);*/
+     
    /* this.route.paramMap.subscribe(pa =>      
       this.orderDataService.getOrder$(pa.get('id'))     
         .subscribe(item => this.order = item)
     ); */
+   // this.printout();
 
   }
+  printout(): void{
 
+      console.log("PRINTING FROM THE PRINTOUT METHOD --------------");
+      console.log("printing current user");
+      console.log(this.currentUser);
+      console.log("printing order");
+      console.log(this.order);
+      console.log("printing product");
+      console.log(this.product);
+  }
+  get orderlines$(): Orderline[]{
+    return this.order.orderLines;
+  }
 }
--- client/shopaholic/src/app/orderline/orderline.component.ts
+++ client/shopaholic/src/app/orderline/orderline.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductDataService } from '../product/product-data.service';
+import { Product } from '../product/product.model';
+import { Orderline } from './orderline.model';
 
 @Component({
   selector: 'app-orderline',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orderline.component.css']
 })
 export class OrderlineComponent implements OnInit {
+  @Input() public orderLine: Orderline;
+  //@Input() public product: Observable<Product>;
 
-  constructor() { }
+  constructor(private _productdataservice: ProductDataService
+    //, private product: string
+    ) { 
+    //this.product =  this._productdataservice.getProduct$(productId);
+  }
 
   ngOnInit(): void {
   }
+
+  
 
 }

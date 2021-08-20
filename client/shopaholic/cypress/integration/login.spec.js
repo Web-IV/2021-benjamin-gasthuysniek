f7395c52f9dd117f9b21ea87f2569e3beb06e0db
= <=>
2021-08-20 23:49:04 +0200

    adding cypress test

---
+++ client/shopaholic/cypress/integration/login.spec.js
@@ -0,0 +1,22 @@
+describe('Login Test', function(){ //describe structures multiple tests
+  
+    it('login', function(){
+        cy.server({delay: 1000}); //the requests will pass throught this server
+       
+        
+        cy.visit('/');
+        
+        //baseurl from cypress.json
+        cy.get('[data-cy=login-email]').should('have.length',1);
+        cy.get('[data-cy=login-password]').should('have.length',1);
+        cy.get('[data-cy=login-email]').type('niek.gasthuys.y9891@student.hogent.be');
+        cy.get('[data-cy=login-password]').type('Nieker@12345');
+        cy.get('[data-cy=login-button]').should('be.visible');       
+
+  }
+)
+        
+    });
+   
+    
+
